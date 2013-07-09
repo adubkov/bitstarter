571485c5e9a212ad2d2ae5089f13d2ce2c92b3cc
Alexey Dubkov <alexey.dubkov@gmail.com>
2013-07-09 13:51:11 +0400

    Test

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	fs.readFile('./index.html', function(err, data) {
+	fs.readFile('index.html', function(err, data) {
 	if (err) {
-		 console.log("Error while reading file");
+		response.send("Error while reading file");
 	} else {
 		response.send(date);
 	}

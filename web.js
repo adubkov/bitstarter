99cc7e705fb1a9b7aef4a8ac06392540d082cbaa
Alexey Dubkov <alexey.dubkov@gmail.com>
2013-07-09 13:58:28 +0400

    Test

--- web.js
+++ web.js
@@ -4,11 +4,12 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+
 	fs.readFileSync('./index.html', function(err, data) {
 		if (err) {
 			response.send("Error while reading file");
 		} else {
-			response.send(data);
+			response.send(data.toString('utf8',0,data.length));
 		}
 	});
 
